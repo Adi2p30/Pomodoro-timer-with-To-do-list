eee65a134bfc948e450232c713d89f69c2671bb3
Rixen <Rixenlama@gmail.com>
2023-03-05 19:56:53 +0200

    Fix of little tiny bug

--- Js/main.js
+++ Js/main.js
@@ -1,7 +1,6 @@
 const urlBackgroundInput = document.querySelector("#background-url-input");
 
 urlBackgroundInput.addEventListener("input", () => {
-  console.log("input");
   document.body.style[
     "background-image"
   ] = `url("${urlBackgroundInput.value}")`;
