9c4441b33dca9ff7a60e9bfc8f9957e441d99413
Rixen <Rixenlama@gmail.com>
2023-03-08 19:48:36 +0200

    New task-list

---
+++ Js/task-list.js
@@ -0,0 +1,27 @@
+let taskValue = document.getElementById("tasklist-item");
+let ul = document.getElementById("tasklist-ul")
+
+const createTask = () => {
+    if (taskValue.value === ""){
+        alert("Write something");
+    } else {
+    var task = document.createElement("p");
+    task.innerText = taskValue.value;
+    var listItem = document.createElement("li");
+    listItem.classList.add("tasklist-li");
+    listItem.appendChild(task);
+    ul.appendChild(listItem); 
+    task.innerhtml = taskValue.value;
+    taskValue.value = "";
+    }
+
+    listItem.addEventListener ("click",  function (){
+        task.style.textDecoration = "line-through";
+    })
+    listItem.addEventListener ("dblclick",  function (){
+        ul.removeChild(listItem);
+    })
+}
+
+
+
