e942c8c1a546738256ac2e57ec134fa55af6e4b3
Rixen <Rixenlama@gmail.com>
2023-03-14 07:11:23 +0200

    + working timer + working controls - Problem with timer values

--- Js/timer.js
+++ Js/timer.js
@@ -3,7 +3,22 @@ let pause = document.querySelector("#pause")
 let reset = document.querySelector("#reset")
 let timeSeconds = document.querySelector("#timer-number-sec")
 let timeMinutes = document.querySelector("#timer-number-min")
+let time = timeMinutes.value * 60;
 
+const timerUpdate = () => {
+    let minutes = Math.floor(time/60);
+    let seconds = time % 60;
+    
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+    timeMinutes.value = minutes;
+    timeSeconds.value = seconds;
+
+    if(time === 0){
+        clearInterval (movingTime)
+    }
+    time--;
+}
 
 
 const startTimer = () => {
@@ -13,6 +28,7 @@ const startTimer = () => {
         reset.style.opacity = "1";
         timeSeconds.opacity = "1";
         timeMinutes.opacity = "1";
+        movingTime = setInterval (timerUpdate, 1000)
 }
 
 const pauseTimer = () =>{
@@ -22,6 +38,9 @@ const pauseTimer = () =>{
     reset.style.opacity = "0.1";
     timeSeconds.opacity = "0.2";
     timeMinutes.opacity = "0.2";
+    clearInterval (movingTime)
+    timeMinutes.value = minutes;
+    timeSeconds.value = seconds;
 }
 
 const resetTimer = () =>{
@@ -31,8 +50,11 @@ const resetTimer = () =>{
     reset.style.opacity = "0.1";
     timeSeconds.opacity = "0.2";
     timeMinutes.opacity = "0.2";
+    clearInterval (movingTime)
     timeSeconds.value = "00";
     timeMinutes.value = "25";
+    time = timeMinutes.value * 60;
+
 }
 
 play.addEventListener("click", startTimer)
