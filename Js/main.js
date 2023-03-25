45a5d9e159038e27cd1fda870048e77b1809efd5
Rixen <Rixenlama@gmail.com>
2023-03-25 16:16:40 +0200

    New Sounds testor system + From maggu

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,8 @@
 // Background
-
+let openNavBarAnimationInterval;
+var openNavBarAnimationValue = 0;
 const urlBackgroundInput = document.querySelector("#background-url-input");
+const soundTesting = document.querySelector("#soundTesting")
 
 urlBackgroundInput.addEventListener("input", () => {
   document.body.style[
@@ -14,9 +16,34 @@ urlBackgroundInput.addEventListener("input", () => {
 const hideNavBar = () => {
   document.querySelector("#nav-bar").style.display = "none";
   document.querySelector(".open-nav-bar-button").style.display = "flex";
+  clearInterval(openNavBarAnimationInterval);
 };
 
 const openNavBar = () => {
   document.querySelector("#nav-bar").style.display = "flex";
   document.querySelector(".open-nav-bar-button").style.display = "none";
+  openNavBarAnimationInterval = setInterval(openNavBarAnimation, 1000);
 };
+
+
+//Open Nav Bar Button animation
+
+/* const openNavBarAnimation = () =>{
+  if(openNavBarAnimationValue =0){
+    document.querySelector(".open-nav-bar-button").style.transform = "scale(1.5)";
+    openNavBarAnimationValue = openNavBarAnimationValue++;
+  } else if (openNavBarAnimationValue = 1){
+    document.querySelector(".open-nav-bar-button").style.transform = "scale(1)";
+    openNavBarAnimationValue = openNavBarAnimationValue--;
+  } else {
+    alert("You got a bug, report it!")
+  }
+  }
+ */
+
+
+
+//Audio Volume tester
+soundTesting.addEventListener("click", () =>{
+  timeUpSound.play()
+})
